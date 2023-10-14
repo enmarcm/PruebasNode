28543f9400232d8e672594a5388d49242d1d95a3
enmarcm <theenmanuel123@gmail.com>
2023-10-14 13:32:58 -0400

    Agrego comentario adicional

--- SessionComponent/src/utils/importJson.js
+++ SessionComponent/src/utils/importJson.js
@@ -2,6 +2,7 @@ import { createRequire } from "node:module";
 
 const require = createRequire(import.meta.url);
 
+//Las rutas siempre deben ser llamada como si estuvieran llamando desde aqui
 const importJson = ({ path }) => require(path);
 
 export default importJson
